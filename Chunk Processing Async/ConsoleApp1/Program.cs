db1e7dfd2b07a377c161b6a3a0ee9b78c555203c
HusamGhannam <husamgamer.2017@gmail.com>
2026-06-28 19:56:47 +0300

    Add files via upload

---
+++ Chunk Processing Async/ConsoleApp1/Program.cs
@@ -0,0 +1,26 @@
+﻿public class Program
+{
+    public async Task ProcessDataChuncksAsync(int chunkNumber)
+    {
+        Console.WriteLine($"Processing chunk {chunkNumber}");
+        await Task.Delay(2000);
+        Console.WriteLine($"Finished processing chunk {chunkNumber}");
+    }
+
+    public async Task ProcessDataAsync(int totalChunks)
+    {
+        var tasks = new List<Task>();
+        for (int i = 1; i <= totalChunks; i++)
+        {
+            tasks.Add(ProcessDataChuncksAsync(i));
+        }
+        await Task.WhenAll(tasks);  
+        Console.WriteLine("All chunks processed.");
+    }    
+
+    public async static Task Main(string[] args)
+    {
+        Program program = new Program();
+        await program.ProcessDataAsync(5);
+    }
+}
