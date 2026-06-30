834fd6992fd226cfb4f50cce172ed3dbfff4ff68
HusamGhannam <husamgamer.2017@gmail.com>
2026-06-30 15:04:37 +0300

    Add files via upload

---
+++ DebuggingApp/ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿ class Program
+{
+
+    public static void Main(string[] args)
+    {
+        // Calling the asynchronous method
+        Task.Run(async () => await PerformAsyncOperation()).Wait();
+        Console.WriteLine("Main method completed.");
+    }
+
+        public static async Task PerformAsyncOperation()
+    {
+        try
+        {
+            Console.WriteLine("Starting async operation...");
+            await Task.Delay(2000); // Simulate an asynchronous operation
+            Console.WriteLine("Async operation completed.");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Failed : {ex}");
+        }
+    }
+}
---
+++ Downloading async/DownloadingFileAsynchronous/Program.cs
@@ -0,0 +1,31 @@
+﻿public class Program
+{
+    //identifying the method as asynchronous
+    public async Task<string> DownloadFileAsync(string FileName)
+    {
+        Console.WriteLine("Downloading file: " + FileName);
+        await Task.Delay(2000); // Simulate a delay for downloading
+        Console.WriteLine("File downloaded: " + FileName);
+        return $"{FileName} content.";
+    }
+    //Using the async method
+    public async Task StartingDownloadAsync()
+    {
+        //we call Download file to download two files asynchronously
+        var downloadTask1 = DownloadFileAsync("file1.txt");
+        var downloadTask2 = DownloadFileAsync("file2.txt");
+        //wait for both downloads to complete
+        await Task.WhenAll(downloadTask1, downloadTask2);
+        Console.WriteLine("All files downloaded.");
+    }
+
+
+
+
+    public static async Task Main(string[] args)
+    {
+        Program program = new Program();
+        //Calling the async method from Main
+        await program.StartingDownloadAsync();
+    }
+}
