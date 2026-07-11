c3256399484e47776adb405c31a1bcc0e04f2c46
HusamGhannam <husamgamer.2017@gmail.com>
2026-07-11 12:55:59 -0700

    Delete CarDealership directory

--- CarDealership/Domain/Events/SellCar.cs
+++
@@ -1,4 +0,0 @@
-namespace Domain.Events
-{
-    public record CarSoldEvent(int CarId, string Brand, string Model, string Color, int Year, decimal Price, DateTime SoldAt, int SellerId, int CustomerId);
-}
